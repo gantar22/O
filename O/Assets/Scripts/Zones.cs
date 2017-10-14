b56ffeee4949628b4d37270564d4410adee7613d
Siri Tembunkiart <botbunk@gmail.com>
2017-10-14 18:44:17 -0400

    Adde (nonfunctional) zones script
    + placeholder image

---
+++ O/Assets/Scripts/Zones.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zones : MonoBehaviour {
+
+	private PlayerMovement playerEnteredInput;
+	private PlayerMovement playerOtherInput;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void onTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player1")
+		{
+
+		}
+		if (other.tag == "Player2")
+		{
+
+		}
+	}
+}
