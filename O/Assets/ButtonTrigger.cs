3b3967097ed0c3c96d79c05b4308186910d51c04
Nathan Carter Williams <nathancarterwilliams@gmail.com>
2017-10-22 14:02:17 -0400

    some changes

---
+++ O/Assets/ButtonTrigger.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTrigger : MonoBehaviour {
+
+	public List<string> mappingNames;
+	public string callName;
+	public bool playerSpecific;
+	public bool switched;
+	public List<string> triggerList;
+	public List<string> untriggerList;
+	string one;
+	string two;
+
+	public void trigger() {
+		if (switched) {
+			string one = "_2";
+			string two = "_1";
+		} else {
+			string one = "_1";
+			string two = "_2";
+		}
+		if (playerSpecific) {
+			if (coll.name == "Player1"){
+				foreach (string name in mappingNames)
+					EventManager.TriggerEvent(name + one);
+			}
+			if (coll.name == "Player2"){
+				foreach (string name in mappingNames)
+					EventManager.TriggerEvent(name + two);
+			} 
+			EventManager.TriggerEvent(callName);
+		} else {
+			EventManager.TriggerEvent(callName);
+			foreach (string name in mappingNames)
+				EventManager.TriggerEvent(name);
+		}
+		foreach (string name in triggerList)
+			EventManager.TriggerEvent(name);
+	}
+
+	public void untrigger() {
+		if (switched) {
+			string one = "_2";
+			string two = "_1";
+		} else {
+			string one = "_1";
+			string two = "_2";
+		}
+		if (playerSpecific) {
+			if (coll.name == "Player1"){
+				foreach (string name in mappingNames)
+					EventManager.TriggerEvent(name + "_undo" + one);
+			}
+			if (coll.name == "Player2"){
+				foreach (string name in mappingNames)
+					EventManager.TriggerEvent(name + "_undo" + two);
+			} 
+			EventManager.TriggerEvent(callName);
+		} else {
+			EventManager.TriggerEvent(callName);
+			foreach (string name in mappingNames)
+				EventManager.TriggerEvent(name + "undo");
+		}
+		foreach (string name in untriggerList)
+			EventManager.TriggerEvent(name);
+	}
+
+}
--- O/Assets/button.cs
+++ O/Assets/button.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class button : MonoBehaviour {
 
+	private bool pressed;
+	private int n;
+
 	// Use this for initialization
 	void Start () {
+		pressed = false;
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void OnCollisionExit2D(Collision2D coll) {
+		if (pressed) {
+
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.relativeVelocity[1] < -.5f && !pressed) {
+			pressed = false;
+			ButtonTrigger triggerer = GetComponent<ButtonTrigger>();
+			triggerer.trigger();
 	}
 }
