417b1409897d9db4394a0187c1e25ff98282d4c8
Matthew Bofenkamp <mbofenka@andrew.cmu.edu>
2017-11-18 18:27:50 -0500

    Player movement bug fixed

--- O/Assets/Scripts/PlayerMovementTemp.cs
+++ O/Assets/Scripts/PlayerMovementTemp.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovementTemp : MonoBehaviour {
 
 	public KeyCode left;
 	public KeyCode right;
