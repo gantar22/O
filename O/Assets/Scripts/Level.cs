157d233077edfcf539dc7657901a0869649b912a
Nathan Carter Williams <nathancarterwilliams@gmail.com>
2017-11-18 21:30:52 -0500

    added design notes

--- O/Assets/Scripts/Level.cs
+++ O/Assets/Scripts/Level.cs
@@ -5,8 +5,12 @@ using System;
 [Serializable]
 public class Level : ScriptableObject {
 
-	public LevelObject[] components;
+	[TextArea(7,20)]
+	public string designNotes;
+
 	public float cameraSize = 5; //default so you don't get an error with default levels;
 	public bool dynamicCam = false;
+
+	public LevelObject[] components;
 
 }
