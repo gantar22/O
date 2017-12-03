d099c7dc97e342c2e11fb748c3433668263cb6e6
Dmcdominic <dmcdominic@gmail.com>
2017-12-03 16:54:01 -0500

    Pausing, and Keyboard Compatible Movement

---
+++ O/Assets/Scripts/util/PauseManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseManager : MonoBehaviour {
+
+	public GameObject PausePanel;
+	private bool paused = false;
+
+	void Update () {
+		// Pause keys
+		if (Input.GetKeyDown (KeyCode.Escape) || Input.GetKeyDown (KeyCode.Keypad7)) {
+			if (paused) {
+				resume ();
+			} else {
+				pause ();
+			}
+		}
+
+		// Menu navigation
+
+	}
+
+	public void pause() {
+		paused = true;
+		Time.timeScale = 0f;
+		PausePanel.SetActive (true);
+	}
+
+	public void resume() {
+		paused = false;
+		Time.timeScale = 1.0f;
+		PausePanel.SetActive (false);
+	}
+}
