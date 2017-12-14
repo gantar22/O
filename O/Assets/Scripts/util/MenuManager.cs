c0ef27e5e5977630d36a572d0e6fb502430d9be8
Dmcdominic <dmcdominic@gmail.com>
2017-12-14 13:39:56 -0500

    Wrapping in the Menu UI

--- O/Assets/Scripts/util/MenuManager.cs
+++ O/Assets/Scripts/util/MenuManager.cs
@@ -17,7 +17,16 @@ public class MenuManager : MonoBehaviour {
 	public Slider SFXVolumeSlider;
 
 	private bool buttonSelected;
+	private GameObject prevSelected;
+
 	private bool delayed;
+	private int lastDirection;
+	private float delayTimer;
+	private float holdTimer;
+
+	private float slowDelay = 0.65f;
+	private float fastDelay = 0.14f;
+	private float currentDelay;
 
 	void OnEnable() {
 		if (FullscreenToggle != null) {
@@ -35,15 +44,21 @@ public class MenuManager : MonoBehaviour {
 			MusicVolumeSlider.onValueChanged.AddListener (delegate {OnMusicVolumeChange ();} );
 			SFXVolumeSlider.onValueChanged.AddListener (delegate {OnSFXVolumeChange ();} );
 		}
+
+		resetDelay ();
 	}
 
 	void Update() {
 		if (EventSystem == null)
 			Debug.LogError("Missing event system in MenuManager script");
+		
+		if (EventSystem.currentSelectedGameObject == null) {
+			buttonSelected = false;
+		}
 
+		// Default select the top or bottom of the menu
 		float vertical = Input.GetAxisRaw ("Vertical");
 		if ((vertical == -1 || vertical == 1) && !buttonSelected) {
-			print (vertical);
 			if (vertical == -1) {
 				EventSystem.SetSelectedGameObject (defaultSelect);
 			} else if (vertical == 1) {
@@ -53,23 +68,58 @@ public class MenuManager : MonoBehaviour {
 			buttonSelected = true;
 		}
 
-		/* } else if (vertical == -1 || vertical == 1) {
-			if (EventSystem.currentSelectedGameObject == defaultSelect && vertical == 1) {
-				Invoke(()=>buttonSelected = false,0.2f);
-				//buttonSelected = false;
-			} else if (EventSystem.currentSelectedGameObject == defaultUpSelect && vertical == -1) {
-				Invoke(()=>buttonSelected = false,0.2f);
+		// Wrapping top-to-bottom and bottom-to-top
+		if (prevSelected != EventSystem.currentSelectedGameObject) {
+			delayed = true;
+			delayTimer = 0;
+		}
+		if (vertical != 0) {
+			lastDirection = (int)vertical;
+		}
+
+		if ((vertical == -1 || vertical == 1) && buttonSelected) {
+			if (prevSelected != EventSystem.currentSelectedGameObject) {
+				delayed = true;
+				delayTimer = 0;
 			}
-		} */
+
+			if ((int)vertical == lastDirection) {
+				delayTimer += Time.deltaTime;
+				holdTimer += Time.deltaTime;
+			} else {
+				resetDelay ();
+			}
+
+			if (holdTimer >= slowDelay) {
+				currentDelay = fastDelay;
+			} else {
+				currentDelay = slowDelay;
+			}
+
+			if (EventSystem.currentSelectedGameObject == defaultSelect && vertical == 1 && (!delayed || (delayTimer >= currentDelay)) ) {
+				EventSystem.SetSelectedGameObject (defaultUpSelect);
+				delayTimer = 0;
+			} else if (EventSystem.currentSelectedGameObject == defaultUpSelect && vertical == -1 && (!delayed || (delayTimer >= currentDelay)) ) {
+				EventSystem.SetSelectedGameObject (defaultSelect);
+				delayTimer = 0;
+			}
+
+		} else {
+			resetDelay ();
+		}
+
+		prevSelected = EventSystem.currentSelectedGameObject;
+	}
+
+	private void resetDelay() {
+		delayTimer = 0;
+		holdTimer = 0;
+		lastDirection = 2;
+		delayed = false;
 	}
 
 	private void OnDisable() {
 		buttonSelected = false;
-		delayed = false;
-	}
-
-	private void undelay() {
-		delayed = false;
 	}
 
 	public void OnFullscreenToggle() {
